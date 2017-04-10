253b4934d5d92449312065966e1436c182988f0d
xiongliang.xl <xiongliang.xl@alibaba-inc.com>
2017-04-10 10:45:32 +0800

    init

--- winston-simpledb.js
+++ winston-simpledb.js
@@ -1,22 +1,9 @@
-// --------------------------------------------------------------------------------------------------------------------
-//
-// winston-simpledb.js : transport for logging to Amazon SimpleDB
-//
-// Author           : Andrew Chilton
-// Web              : http://www.chilts.org/blog/
-// Email            : <chilts@appsattic.com>
-//
-// Copyright (c)    : 2011 AppsAttic Ltd
-// Web              : http://www.appsattic.com/
-// License          : http://opensource.org/licenses/MIT
-//
-// --------------------------------------------------------------------------------------------------------------------
-
 var util = require('util');
-var amazon = require('awssum/lib/amazon/amazon');
-var simpledb = require('awssum/lib/amazon/simpledb');
+var aliyun = require('aliyun-sdk');
+var simpledb = require('awssum/lib/aliyun/simpledb');
 var winston = require('winston');
 var UUID = require('uuid-js');
+var common = require();
 
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -25,53 +12,53 @@ var UUID = require('uuid-js');
 // Constructor for the SimpleDB transport object.
 //
 var SimpleDB = exports.SimpleDB = function (options) {
-    options = options || {};
+  options = options || {};
 
-    // need the accessKeyId
-    if (!options.accessKeyId) {
-        throw new Error("Required: accessKeyId for the Amazon account to log for.");
+  // need the accessKeyId
+  if (!options.accessKeyId) {
+    throw new Error("Required: accessKeyId for the aliyun account to log for.");
+  }
+
+  // need the secretAccessKey
+  if (!options.secretAccessKey) {
+    throw new Error("Required: secretAccessKey for the aliyun account being used.");
+  }
+
+  // need the projectName
+  if (!options.projectName) {
+    throw new Error("Required: projectName (or projectName generator) to log to.");
+  }
+
+  // need the endpoint
+  if (!options.endpoint) {
+    throw new Error("Required: endpoint the domain is in.");
+  }
+
+  // Winston Options
+  this.name = 'sls';
+  this.level = options.level || 'info';
+
+  // SimpleDB Options
+  if (options.projectName) {
+    this.projectName = options.projectName;
+  }
+  this.itemName = this.itemName || 'timestamp';
+  if (options.itemName) {
+    this.itemName = options.itemName;
+  }
+
+  // create the SimpleDB instance
+  this.sdb = new aliyun.SLS({
+    accessKeyId: options.accessKeyId,
+    secretAccessKey: options.secretAccessKey,
+    endpoint: options.endpoint,
+    apiVersion: '2015-06-01'
+
+    //以下是可选配置
+    , httpOptions: {
+      timeout: 2000
     }
-
-    // need the secretAccessKey
-    if (!options.secretAccessKey) {
-        throw new Error("Required: secretAccessKey for the Amazon account being used.");
-    }
-
-    // need the awsAccountId
-    if (!options.awsAccountId) {
-        throw new Error("Required: awsAccountId for the Amazon account being used.");
-    }
-
-    // need the domainName
-    if (!options.domainName) {
-        throw new Error("Required: domainName (or domainName generator) to log to.");
-    }
-
-    // need the region
-    if (!options.region) {
-        throw new Error("Required: region the domain is in.");
-    }
-
-    // Winston Options
-    this.name  = 'simpledb';
-    this.level = options.level || 'info';
-
-    // SimpleDB Options
-    if (options.domainName) {
-        this.domainName = options.domainName;
-    }
-    this.itemName = this.itemName || 'timestamp';
-    if (options.itemName) {
-        this.itemName = options.itemName;
-    }
-
-    // create the SimpleDB instance
-    this.sdb = new simpledb.SimpleDB(
-        options.accessKeyId,
-        options.secretAccessKey,
-        options.awsAccountId,
-        options.region
-    );
+  });
 };
 
 //
@@ -88,65 +75,60 @@ util.inherits(SimpleDB, winston.Transport);
 // Core logging method exposed to Winston. Metadata is optional.
 //
 SimpleDB.prototype.log = function (level, msg, meta, callback) {
-    var self = this;
+  if (this.silent) {
+    return callback(null, true);
+  }
 
-    // console.log('RIGHT HERE - logging some stuff');
+  var self = this;
+  var output = [
+    {
+      key: 'level',
+      value: level
+    },
+    {
+      key: 'message',
+      value: stringify(msg) || ''
+    }
+  ];
 
-    // create the domainName
-    var domainName;
-    if ( typeof this.domainName === 'function' ) {
-        domainName = this.domainName();
+  sls.putLogs({
+    //必选字段
+    projectName: this.projectName,
+    logStoreName: this.logStoreName,
+    logGroup: {
+      logs: [{
+        time: Math.floor(new Date().getTime() / 1000),
+        contents: output
+      }],
+      topic: meta || meta.topic || '',
+      source: meta || meta.source || ''
     }
-    else {
-        domainName = this.domainName;
-    }
-    // console.log('domainName=' + domainName);
+  }, function (err, data) {
 
-    // create the itemName
-    var itemName;
-    if ( typeof this.itemName === 'function' ) {
-        itemName = this.itemName();
-    }
-    else if ( this.itemName === 'uuid' ) {
-        itemName = UUID.create().hex;
-    }
-    else if ( this.itemName === 'epoch' ) {
-        itemName = Date.now();
-    }
-    else if ( this.itemName === 'timestamp' ) {
-        itemName = (new Date()).toISOString();
-    }
-    // else, nothing (should never be here)
-
-    // create the data to log
-    var data = {
-        level     : level,
-        msg       : msg,
-        inserted  : (new Date()).toISOString(),
-    };
-
-    // add the meta information if there is any
-    if ( meta ) {
-        data.meta = JSON.stringify(meta);
+    if (err) {
+      self.emit('error', err);
+      return;
     }
 
-    // store the message
-    this.sdb.putAttributes({
-        domainName : domainName,
-        itemName   : itemName,
-        data       : data,
-    }, function(err, data) {
-        // console.log('Error: ', util.inspect(err, true, null));
-        // console.log('Data: ', util.inspect(data, true, null));
-        if (err) {
-            self.emit('error', err);
-        }
+    self.emit('logged');
+  });
 
-        self.emit('logged');
-    });
+  // intially, tell the caller that everything was fine
+  callback(null, true);
+};
 
-    // intially, tell the caller that everything was fine
-    callback(null, true);
+const stringify = function (s) {
+  return JSON.stringify(s, function (key, value) {
+    if (value instanceof Buffer) {
+      return value.toString('base64');
+    }
+
+    if (value instanceof Error) {
+      return value.stack;
+    }
+
+    return value;
+  })
 };
 
 //
