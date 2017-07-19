6bdc85b40098bd3681c1e6eb3877c531ce6751b6
xiongliang.xl <xiongliang.xl@alibaba-inc.com>
2017-07-19 10:48:37 +0800

    0.8.5

--- winston-sls.js
+++ winston-sls.js
@@ -142,8 +142,14 @@ SLS.prototype.log = function (level, msg, meta, callback) {
           self.timeoutId = null;
 
           if (err) {
-            console.log('winston sls error', err, self.projectName, self.logStoreName, logGroup);
-            self.emit('error', err);
+            // console.log('winston sls error', err, self.projectName, self.logStoreName, logGroup);
+            self.emit('error', err, self.projectName, self.logStoreName, logGroup);
+            self.emit('error', {
+              err: err,
+              projectName: self.projectName,
+              logStoreName: self.logStoreName,
+              logGroup: logGroup
+            });
             return;
           }
 
